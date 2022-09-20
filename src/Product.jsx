0a1693bbf2c8aea889d61a7e442fb2be3b9a59f7
sammie kihara <sammiekihara09@gmail.com>
2022-09-20 10:44:12 +0300

    second update

--- src/Product.jsx
+++ src/Product.jsx
@@ -2,6 +2,7 @@ import React,{Component} from "react";
 
 export default class Product extends Component {
     render(){
+        console.log(this.props)
         return <div>Product #{this.props.id}</div>
     }
 }
--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -1,33 +1,30 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import Product from "./Product";
 
 export default class ShoppingCart extends Component {
-    state ={
-        products: [
-            {id:1, productName:"iPhone", price:8900, quantity:0},
-            {id:2, productName:"Sony Camera", price:4500, quantity:0},
-            {id:3, productName:"Samsung QLED TV", price:77400, quantity:0},
-            {id:4, productName:"iPad Pro", price:12400, quantity:0},
-            {id:5, productName:"Xbox", price:7780, quantity:0},
-            {id:6, productName:"Dell Monitor", price:890, quantity:0},
+  state = {
+    products: [
+      { id: 1, productName: "iPhone", price: 8900, quantity: 0 },
+      { id: 2, productName: "Sony Camera", price: 4500, quantity: 0 },
+      { id: 3, productName: "Samsung QLED TV", price: 77400, quantity: 0 },
+      { id: 4, productName: "iPad Pro", price: 12400, quantity: 0 },
+      { id: 5, productName: "Xbox", price: 7780, quantity: 0 },
+      { id: 6, productName: "Dell Monitor", price: 890, quantity: 0 },
+    ],
+  };
 
+  render() {
+    return (
+      <div className="container-fluid">
+        <h4> Shopping cart</h4>
 
-        ]
-    }
-
-    render(){
-        return (<div>
-            <h4> Shopping cart</h4>
-            
-            <div>
-                {this.state.products.map((x) =>{
-                   let {id, productName, price, quantity} = x
-                    return <Product key={id} id={price} />
-                })}
-            </div>
-
-            </div>)
-    }
-
-
+        <div className="row">
+          {this.state.products.map((x) => {
+            let { id, productName, price, quantity } = x;
+            return <Product key={id} id={id} />;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
